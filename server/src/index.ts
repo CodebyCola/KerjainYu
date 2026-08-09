d68550b47e68abc96fa67a387e6bf1c164cc4777
Mike <michaelbintang2006@gmail.com>
2026-08-09 12:06:35 +0700

    nothing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,33 @@
-import 'dotenv/config'
-import express from 'express'
-import { errorHandler } from './middlewares/errorHandler'
-import { db } from './database/db'
-import cookieParser from 'cookie-parser'
-import authRoutes from "./routes/auth.route"
-import projectRoutes from "./routes/project.route"
-import cors from "cors"
+import "dotenv/config";
+import express from "express";
+import { errorHandler } from "./middlewares/errorHandler";
+import { db } from "./database/db";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route";
+import projectRoutes from "./routes/project.route";
+import cors from "cors";
 
 const app = express();
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    }),
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
 );
 
-app.get('/health', (req, res) => {
-    res.json({ success: true, message: "Server is running" })
-})
+app.get("/health", (req, res) => {
+  res.json({ success: true, message: "Server is running" });
+});
 // Other Routes
-app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/projects", projectRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/projects", projectRoutes);
 
-app.use(errorHandler)
-const PORT = process.env.PORT || 5000
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
 
-db.raw('SELECT 1')
-    .then(() => console.log('Database connected'))
-    .catch((err) => console.error('Database connection failed:', err))
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
+db.raw("SELECT 1")
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error("Database connection failed:", err));
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
