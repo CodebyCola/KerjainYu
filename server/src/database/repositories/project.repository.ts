17e0c05838837dcf7b52cf34592c9af4129fce65
MichaelB232 <michaelbintang2006@gmail.com>
2026-08-09 09:08:23 +0700

    On Progress feature/project

---
+++ server/src/database/repositories/project.repository.ts
@@ -0,0 +1,17 @@
+import { db } from "../db";
+import { Knex } from "knex";
+
+
+export async function createProject(data: { title: string, allowFreeSwap: boolean, deadline: Date }, trx?: Knex.Transaction) {
+    const executor = trx || db
+    return executor("projects").insert(data).returning("*")
+}
+
+export async function updateProject(data: Partial<{ title: string, allowFreeSwap: boolean, status: string, deadline: Date, isArchived: boolean, }>) {
+    return db("projects").update(data).returning("*")
+}
+// export async function getAllProjectByUserId(userId: number, role: string) {
+//     returning db("projects")
+// }
+
+// export async function 
