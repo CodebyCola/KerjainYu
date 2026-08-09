ca9411eb974346757fc318054f5aa8d9cb9aeb62
MichaelB232 <michaelbintang2006@gmail.com>
2026-08-09 12:36:00 +0700

    Fixing PORT

--- server/src/docs/swagger.ts
+++ server/src/docs/swagger.ts
@@ -2,9 +2,9 @@ import { OpenApiGeneratorV3 } from "@asteasolutions/zod-to-openapi";
 import { registry } from "./registry";
 
 const generator = new OpenApiGeneratorV3(registry.definitions);
-
+const PORT = process.env.PORT
 export const swaggerSpec = generator.generateDocument({
   openapi: "3.0.0",
   info: { title: "KerjainYu API", version: "1.0.0" },
-  servers: [{ url: "http://localhost:4000" }],
+  servers: [{ url: `http://localhost:${PORT}` }],
 });
